6bac42272841367948ab977f3607940b5b91a7d6
fahmifn.idn@gmail.com <fahmifn.idn@gmail>
2025-05-08 10:02:35 +0700

    4.2

--- resources/views/user/laporan/index.blade.php
+++ resources/views/user/laporan/index.blade.php
@@ -1,15 +1,54 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+            Halaman Laporan
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    Anda user
+                <div class="p-6 flex justify-between">
+                    <div class="">
+                        <h2 class="font-semibold text-xl dark:text-white mb-1">Laporan saya</h2>
+                        <span class="dark:text-gray-300"> Data laporan saya yang sudah diajukan. </span>
+                    </div>
+                    <div class="">
+                        <a href="" class="text-red-600 font-semibold hover:text-red-500">Ajukan</a>
+                    </div>
+                </div>
+                <div class="p-6">
+                    <div class="overflow-x-auto">
+                        <table class="min-w-full">
+                            <thead class="border border-collapse border-x-0 border-gray-300 bg-red-400">
+                                <th class="py-2 px-4 dark:text-gray-300 uppercase text-sm">Judul Laporan</th>
+                                <th class="py-2 px-4 dark:text-gray-300 uppercase text-sm">Status</th>
+                                <th class="py-2 px-4 dark:text-gray-300 uppercase text-sm">Tanggal Laporan</th>
+                            </thead>
+                            <tbody>
+                                <tr class="border border-collapse border-x-0 border-gray-300">
+                                    <td class="py-2 px-4 dark:text-gray-300 uppercase text-xs">TV Pecah</td>
+                                    <td class="py-2 px-4 dark:text-gray-300 uppercase text-xs">pending</td>
+                                    <td class="py-2 px-4 dark:text-gray-300 uppercase text-xs">8/5/2025</td>
+                                </tr>
+                                <tr class="border border-collapse border-x-0 border-gray-300">
+                                    <td class="py-2 px-4 dark:text-gray-300 uppercase text-xs">TV Pecah</td>
+                                    <td class="py-2 px-4 dark:text-gray-300 uppercase text-xs">pending</td>
+                                    <td class="py-2 px-4 dark:text-gray-300 uppercase text-xs">8/5/2025</td>
+                                </tr>
+                                <tr class="border border-collapse border-x-0 border-gray-300">
+                                    <td class="py-2 px-4 dark:text-gray-300 uppercase text-xs">TV Pecah</td>
+                                    <td class="py-2 px-4 dark:text-gray-300 uppercase text-xs">pending</td>
+                                    <td class="py-2 px-4 dark:text-gray-300 uppercase text-xs">8/5/2025</td>
+                                </tr>
+                                <tr class="border border-collapse border-x-0 border-gray-300">
+                                    <td class="py-2 px-4 dark:text-gray-300 uppercase text-xs">TV Pecah</td>
+                                    <td class="py-2 px-4 dark:text-gray-300 uppercase text-xs">pending</td>
+                                    <td class="py-2 px-4 dark:text-gray-300 uppercase text-xs">8/5/2025</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
