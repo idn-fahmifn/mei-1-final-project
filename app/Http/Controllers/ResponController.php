b8f47a74630ba28d3f77ad0788d8833c9a99d497
fahmifn.idn@gmail.com <fahmifn.idn@gmail>
2025-05-08 16:53:46 +0700

    4.30

--- app/Http/Controllers/ResponController.php
+++ app/Http/Controllers/ResponController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Laporan;
+use App\Models\Respon;
 use Illuminate\Http\Request;
 
 class ResponController extends Controller
@@ -24,4 +25,24 @@ class ResponController extends Controller
         $data = Laporan::findOrFail($param);
         return view('respon.create', compact('data'));
     }
+
+    public function store(Request $request, $param)
+    {
+        $request->validate([
+            'detail_respon' => 'required'
+        ]);
+
+        Respon::create([
+            'id_laporan' => $param,
+            'detail_respon' => $request->detail_respon
+        ]);
+
+        $status = Laporan::findOrFail($param);
+        $status->status = $request->status;
+        $status->save();
+
+        return redirect()->route('admin.laporan.detail', $param);
+
+    }
+
 }
